2dd79fe7f1f378eee982b23145ac8195ff623628
carli <carli@DESKTOP-QTFQT3F>
2022-05-25 13:23:18 -0500

    Agregar archivos de proyecto.

---
+++ Lab10/Lab10/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab10
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void GoToViewToViewDemo(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ViewToViewDemo());
+        }
+        private void GoToBindingModeDemo(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new BindingModeDemo());
+        }
+        private void GoToListViewDemo(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ListViewDemo());
+        }
+        private void GoToPickerDemo(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PickerDemo());
+        }
+    }
+}
